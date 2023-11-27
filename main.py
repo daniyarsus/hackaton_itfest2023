42b9d34301619f3e4c31162ec333f99040e6e53e
daniyarsus <daniyar.kanu@gmail.com>
2023-11-28 02:57:54 +0600

    upd

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ from sqlalchemy.orm import sessionmaker
 import jwt
 from fastapi.middleware.cors import CORSMiddleware
 from datetime import datetime, timedelta
-from passlib.context import CryptContext  # Import for password hashing
 
 app = FastAPI()
 
@@ -24,7 +23,6 @@ Base = declarative_base()
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-
 class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True, index=True)
@@ -33,29 +31,22 @@ class User(Base):
     password = Column(String)
     role = Column(String, default="student")
 
-
 Base.metadata.create_all(bind=engine)
 
-
 class UserIn(BaseModel):
-    username: str
+    email: str
     password: str
-
 
 class UserInRegistration(BaseModel):
     username: str
     email: str
     password: str
 
-
 SECRET_KEY = "your_secure_random_key"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
-
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
 
 def create_access_token(data: dict, expires_delta: timedelta = None):
     to_encode = data.copy()
@@ -66,11 +57,6 @@ def create_access_token(data: dict, expires_delta: timedelta = None):
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
-
-
-def get_password_hash(password):
-    return pwd_context.hash(password)
-
 
 async def get_current_user(token: str = Depends(oauth2_scheme)):
     try:
@@ -89,7 +75,6 @@ async def get_current_user(token: str = Depends(oauth2_scheme)):
 
     return user
 
-
 @app.post("/register")
 async def register(user_in: UserInRegistration):
     session = SessionLocal()
@@ -100,32 +85,31 @@ async def register(user_in: UserInRegistration):
     if session.query(User).filter(User.email == user_in.email).first() is not None:
         raise HTTPException(status_code=400, detail="Email already registered")
 
-    hashed_password = get_password_hash(user_in.password)
-    user = User(username=user_in.username, email=user_in.email, password=hashed_password)
+    user = User(username=user_in.username, email=user_in.email, password=user_in.password)
 
     session.add(user)
     session.commit()
 
     return {"username": user.username, "email": user.email}
 
-
 @app.post("/token")
 async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
     session = SessionLocal()
 
+    # Аутентификация с использованием электронной почты
     user = session.query(User).filter(
-        (User.username == form_data.username) | (User.email == form_data.username)
+        User.email == form_data.username
     ).first()
 
-    if not user or not pwd_context.verify(form_data.password, user.password):
+    if not user or user.password != form_data.password:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid username or password",
+            detail="Неверный имейл или пароль",
             headers={"WWW-Authenticate": "Bearer"},
         )
 
     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    access_token = create_access_token(data={"sub": user.username}, expires_delta=access_token_expires)
+    access_token = create_access_token(data={"sub": user.email}, expires_delta=access_token_expires)
 
     return {"access_token": access_token, "token_type": "bearer"}
 
