bc4d1aa13294affac3e0d0a17b50050ae8fc61ba
daniyarsus <daniyar.kanu@gmail.com>
2023-11-27 23:28:18 +0600

    upd

--- main.py
+++ main.py
@@ -1,85 +1,15 @@
-from fastapi import FastAPI, Depends, HTTPException, status
-from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
-from pydantic import BaseModel
-from sqlalchemy import create_engine, Column, Integer, String
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-import jwt
-from datetime import datetime, timedelta
+from fastapi import FastAPI
 
 app = FastAPI()
-DATABASE_URL = "sqlite:///./test.db"
-Base = declarative_base()
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-class User(Base):
-    __tablename__ = "users"
-    id = Column(Integer, primary_key=True, index=True)
-    username = Column(String, unique=True, index=True)
-    password = Column(String)
-    role = Column(String)
+@app.get("/")
+def read_root():
+    return {"message": "Hello, World!"}
 
-Base.metadata.create_all(bind=engine)
+@app.get("/items/{item_id}")
+def read_item(item_id: int, query_param: str = None):
+    return {"item_id": item_id, "query_param": query_param}
 
-class UserIn(BaseModel):
-    username: str
-    password: str
-
-SECRET_KEY = "your_secret_key"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
-
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
-
-def create_access_token(data: dict, expires_delta: timedelta = None):
-    to_encode = data.copy()
-    if expires_delta:
-        expire = datetime.utcnow() + expires_delta
-    else:
-        expire = datetime.utcnow() + timedelta(minutes=15)
-    to_encode.update({"exp": expire})
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    return encoded_jwt
-
-async def get_current_user(token: str = Depends(oauth2_scheme)):
-    try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-        username: str = payload.get("sub")
-        if username is None:
-            raise HTTPException(status_code=401, detail="Invalid authentication credentials")
-    except jwt.PyJWTError:
-        raise HTTPException(status_code=401, detail="Invalid authentication credentials")
-    session = SessionLocal()
-    user = session.query(User).filter(User.username == username).first()
-    if user is None:
-        raise HTTPException(status_code=401, detail="Invalid authentication credentials")
-    return user
-
-@app.post("/register")
-async def register(user_in: UserIn):
-    session = SessionLocal()
-    user = User(username=user_in.username, password=user_in.password)
-    session.add(user)
-    session.commit()
-    return {"username": user.username}
-
-@app.post("/token")
-async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends()):
-    session = SessionLocal()
-    user = session.query(User).filter(User.username == form_data.username).first()
-    if not user or user.password != form_data.password:
-        raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Incorrect username or password",
-            headers={"WWW-Authenticate": "Bearer"},
-        )
-    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    access_token = create_access_token(
-        data={"sub": user.username}, expires_delta=access_token_expires
-    )
-    return {"access_token": access_token, "token_type": "bearer"}
-
-@app.get("/users/me")
-async def read_users_me(current_user: User = Depends(get_current_user)):
-    return current_user
+@app.post("/create_item/{item_name}")
+def create_item(item_name: str):
+    return {"item_name": item_name, "message": "Item created successfully"}
